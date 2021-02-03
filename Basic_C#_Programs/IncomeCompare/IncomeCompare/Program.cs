c7106be4b585d1ca2a1f025e5918a615fbaec2bf
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-02 20:53:38 -0500

    Add new Project: Income Comparison
    Uses math operators and comparisons with user input.

---
+++ Basic_C#_Programs/IncomeCompare/IncomeCompare/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace IncomeCompare
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine();
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("--------");
+            Console.WriteLine();
+            Console.WriteLine("Hourly rate?");
+            string person1rate = Console.ReadLine();
+            Console.WriteLine("Hours per week?");
+            string person1hours = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("--------");
+            Console.WriteLine();
+            Console.WriteLine("Hourly rate?");
+            string person2rate = Console.ReadLine();
+            Console.WriteLine("Hours per week?");
+            string person2hours = Console.ReadLine();
+            Console.WriteLine();
+
+            int salary1 = ((Convert.ToInt32(person1rate) * Convert.ToInt32(person1hours)) * 52);
+            int salary2 = ((Convert.ToInt32(person2rate) * Convert.ToInt32(person2hours)) * 52);
+            bool compare = salary1 > salary2;
+
+            Console.WriteLine("================================");
+            Console.WriteLine("Annual Salary of Person 1:");
+            Console.WriteLine("$" + salary1);
+            Console.WriteLine("Annual Salary of Person 2:");
+            Console.WriteLine("$" + salary2);
+            Console.WriteLine();
+
+            Console.WriteLine("Person 1 earns more than Person 2? ");
+            Console.WriteLine(compare);
+            Console.Read();
+
+        }
+    }
+}
