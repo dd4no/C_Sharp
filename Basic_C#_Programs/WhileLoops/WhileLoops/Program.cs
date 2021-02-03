5ea513994b21a7b2aa1e6d53e9bba3a3effeef77
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-03 16:24:33 -0500

    Add new Project: WhileLoops
    Demonstrate looping using while and do-while statements.

---
+++ Basic_C#_Programs/WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            // While Loop
+            Console.WriteLine("Roll a pair of dice and enter the result");
+            int roll = Convert.ToInt32(Console.ReadLine());
+
+            while (roll != 7)
+            {
+                Console.WriteLine("Roll again and enter the result");
+                int newRoll = Convert.ToInt32(Console.ReadLine());
+                roll = newRoll;
+            }
+            Console.WriteLine("You won!!");
+            Console.WriteLine();
+            Console.WriteLine("[][][][][][][][][][][][]");
+            Console.WriteLine();
+
+
+            // Do-while Loop
+            int differentRoll;
+            do
+            {
+                Console.WriteLine("Roll a pair of dice and enter the result");
+                int anotherRoll = Convert.ToInt32(Console.ReadLine());
+                differentRoll = anotherRoll;
+            }
+            while (differentRoll != 7);
+            Console.WriteLine("You won!!");
+            Console.Read();
+
+        }
+    }
+}
