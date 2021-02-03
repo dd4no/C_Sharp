2034aa4be3aac276e4f2e155b0915aecc2c1eb50
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-03 13:33:25 -0500

    Add new Project: Car Insurance Qualifier
    Demostrates boolean operations

---
+++ Basic_C#_Programs/CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("INSURANCE QUALIFIER");
+            Console.WriteLine("...................");
+            Console.WriteLine();
+
+            Console.WriteLine("Please answer the following questions to check your eligibility:");
+            Console.WriteLine();
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int Age = Convert.ToInt32(age);
+
+            Console.WriteLine("Have you ever had a DUI? (Please answer with \"true\" or \"false\")");
+            string dui = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(dui);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            int Tickets = Convert.ToInt32(tickets);
+
+            bool qualified = (Age > 15 && DUI == false && Tickets <= 3);
+
+            Console.WriteLine();
+            Console.WriteLine("=========================");
+            Console.WriteLine();
+            Console.WriteLine("Qualified for insurance?");
+            Console.WriteLine(qualified);
+            Console.Read();
+
+        }
+    }
+}
