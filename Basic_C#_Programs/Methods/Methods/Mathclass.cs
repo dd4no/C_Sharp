e4f1cd6c25f4d0215c1bac651fce943d3e352aaa
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-05 13:45:13 -0500

    Add new Project: Methods
    Demonstrates class creation and calling methods from a seperate file

---
+++ Basic_C#_Programs/Methods/Methods/Mathclass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Mathclass
+    {
+        public static int AddTen(int x)
+        {
+            return x + 10;
+        }
+
+        public static int TimesTen(int y)
+        {
+            return y * 10;
+        }
+
+        public static int MinusTen(int z)
+        {
+            return z - 10;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods/Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+            Console.WriteLine("Enter a number to perform some math on:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int a = Mathclass.AddTen(userNum);
+            Console.WriteLine("Your number plus ten = " + a);
+            int b = Mathclass.TimesTen(userNum);
+            Console.WriteLine("Your number times ten = " + b);
+            int c = Mathclass.MinusTen(userNum);
+            Console.WriteLine("Your number minus ten = " + c);
+
+            Console.ReadLine();
+        }
+    }
+}
