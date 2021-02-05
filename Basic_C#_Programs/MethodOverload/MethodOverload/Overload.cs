a9b3f8f98ee0cdcfc28367c300ec23a65706c7e9
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-05 15:46:17 -0500

    Add new Project: MethodOverload
    Demonstrates overloading a method with different data types.

---
+++ Basic_C#_Programs/MethodOverload/MethodOverload/Overload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Overload
+    {
+        public static int DoubleIt(int x)
+        {
+            return x * 2;
+        }
+
+        public static decimal DoubleIt(decimal x)
+        {
+            return x * 2;
+        }
+
+        public static string DoubleIt(string x)
+        {
+            int y = Convert.ToInt32(x);
+            y = y + y;
+            return y.ToString();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Overload.DoubleIt(5);
+            Console.WriteLine("Doubling 5 gives a value of " + a);
+            decimal b = Overload.DoubleIt(5.25m);
+            Console.WriteLine("Doubling 5.25 gives a value of " + b);
+            string c = Overload.DoubleIt("10");
+            Console.WriteLine("Doubling \"10\" gives a value of " + c);
+
+            Console.ReadLine();
+        }
+    }
+}
