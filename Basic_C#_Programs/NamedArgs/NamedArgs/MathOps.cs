dbae83d0ef2538b78f7ffb778399839229c41286
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-05 21:22:38 -0500

    Add new project: NamedArgs
    Demonstrates use of named arguments and void methods.

---
+++ Basic_C#_Programs/NamedArgs/NamedArgs/MathOps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedArgs
+{
+    class MathOps
+    {
+        public static void DoSomeStuff(int number1, int number2)
+        {
+            number1++;
+            Console.WriteLine("The second argument is {0}", number2);
+        }
+    }
+}
---
+++ Basic_C#_Programs/NamedArgs/NamedArgs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MathOps();
+
+            MathOps.DoSomeStuff( 5, 15 );
+
+            MathOps.DoSomeStuff(number2: 100, number1: 20);
+
+            Console.Read();
+        }
+    }
+}
