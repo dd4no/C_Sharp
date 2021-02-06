1fde0f545c0b851e5f68bf5449e634ee6f1004e9
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-05 20:37:33 -0500

    Add new Project: OptionalParams
    Demonstrates use of optional parameters in methods and the integer method, TryParse, to handle null input from user.

---
+++ Basic_C#_Programs/OptionalParams/OptionalParams/Calculation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParams
+{
+    public class Calculation
+    {
+        public static int DoSomeMath(int numOne, int numTwo = 5)
+        {  
+
+            return numOne + numTwo;
+        }
+          
+    }
+}
---
+++ Basic_C#_Programs/OptionalParams/OptionalParams/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OptionalParams
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result;
+
+            Console.WriteLine("Choose a number:");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Optionally choose a second number:");
+
+            if (int.TryParse(Console.ReadLine(), out int userNum2))
+            {
+                result = Calculation.DoSomeMath(userNum1, userNum2);
+            }
+            else
+            {
+                Console.WriteLine("The default number is 5");
+                result = Calculation.DoSomeMath(userNum1);
+            }          
+
+            Console.WriteLine();
+            Console.WriteLine("Those two numbers added together = {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
