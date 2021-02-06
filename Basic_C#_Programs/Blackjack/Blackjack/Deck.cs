6f21ffebac35db28a6fa3c975e7075cf505fa400
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-06 13:53:03 -0500

    Update Blackjack project
    Made Shuffle() a Deck class method.  Cleaned up unused code and simplified structure.

--- Basic_C#_Programs/Blackjack/Blackjack/Deck.cs
+++ Basic_C#_Programs/Blackjack/Blackjack/Deck.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blackjack
 {
@@ -18,9 +15,9 @@ namespace Blackjack
                 "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
             };
 
-            foreach (string face in Faces)
+            foreach (string suit in Suits)
             {
-                foreach (string suit in Suits)
+                foreach (string face in Faces)
                 {
                     Card card = new Card();
                     card.Suit = suit;
@@ -29,6 +26,26 @@ namespace Blackjack
                 }
             }
         }
+
         public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {             
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+            
+        }
+
     }
 }
