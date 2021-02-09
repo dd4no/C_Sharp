74bbd83ee47effef7990a978b9cb386e2cf95a99
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-09 16:53:31 -0500

    Add new project: OperatorOverload
    Demonstrates overloading operators.

---
+++ Basic_C#_Programs/OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool match = false;
+            if (employee1.ID == employee2.ID)
+            {
+                match = true;
+            }
+            return match;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool match = false;
+            if (employee1.ID != employee2.ID)
+            {
+                match = true;
+            }
+            return match;
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { ID = 666 };
+            Employee employee2 = new Employee() { ID = 666 };
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Employee ID's match");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee ID's do not match");
+                Console.ReadLine();
+            }
+        }
+    }
+}
