44f1c542b563efa56082d7d37520c3ae829fec83
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-09 15:12:45 -0500

    Add new project: Interfaces
    Demonstrates use of interfaces

---
+++ Basic_C#_Programs/Interface/Interface/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Employee : IQuittable
+    {
+        public int ID { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("Employee #" + ID + " quit");
+            Console.Read();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Interface/Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { ID = 567890 };
+            employee.Quit();
+
+        }
+    }
+}
