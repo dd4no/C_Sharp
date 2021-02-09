3208247c899c88574924249b9527755f2ca61e0f
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-09 08:49:55 -0500

    Add new project: SuperclassMethod
    Demonstrates calling inherited method from superclass.

---
+++ Basic_C#_Programs/SuperclassMethod/SuperclassMethod/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperclassMethod
+{
+    public class Employee : Person
+    {
+        int ID { get; set; }
+    }
+}
