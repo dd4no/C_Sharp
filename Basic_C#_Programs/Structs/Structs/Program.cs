86c32699233944af2d3166155a7f3415fc76ea58
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-10 13:50:33 -0500

    Add new project: Structs
    Demonstrates use of structures.

---
+++ Basic_C#_Programs/Structs/Structs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 44.625m;
+            Console.WriteLine(number.Amount);
+            Console.Read();
+        }
+    }
+}
