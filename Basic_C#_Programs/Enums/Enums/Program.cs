0edd131114f3181f73588790318661f21ea72aa7
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-10 10:06:59 -0500

    Add new project: Enums
    Demonstrates use of enums and parsing strings to enum type.

---
+++ Basic_C#_Programs/Enums/Enums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week:");
+                string currentDay = Console.ReadLine();
+                Console.WriteLine();
+                daysOfWeek day = (daysOfWeek)Enum.Parse(typeof(daysOfWeek), currentDay);
+                Console.WriteLine("Today is " + day);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week, or check spelling.");
+                Console.ReadLine();
+            }
+          
+        }
+
+        enum daysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
