fd9f199b8e3e0d6283b8b2522f1b594f6921077c
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-12 17:36:49 -0500

    Add new project: Exceptions
    Demonstration of exception handling using a class derived from the Exception class.

---
+++ Basic_C#_Programs/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your age:");
+
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age == 0 || age < 0)
+                {
+                    throw new NumberException();
+                }
+                int yearBorn = DateTime.Now.Year - age;
+                Console.WriteLine("You were born in {0}", yearBorn);                
+            }
+            catch (NumberException)
+            {
+                Console.WriteLine("Your age can not be zero, or less than zero.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
