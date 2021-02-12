a94f4b1a076bc3a11e2dcaffaebc0a5832353690
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-12 16:24:46 -0500

    Add new project: Extras
    Demonstrations of constants, keyword "var", and constructor chaining.

---
+++ Basic_C#_Programs/Extras/Extras/ConstructIt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extras
+{
+    class ConstructIt
+    {
+        public string Name;
+        public string Color;
+
+        public ConstructIt() : this("Generic object", "colorless")
+        {
+        }
+        
+        public ConstructIt(string name) : this(name, "colorless")
+        {
+        }
+
+        public ConstructIt(string name, string color)
+        {
+            Name = name;
+            Color = color;
+            Console.WriteLine("A {0} {1} was created.", color, name);
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/Extras/Extras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extras
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string myName = "My name is Dave Defourneaux, and that's not going to change.";
+            Console.WriteLine("Some things are constant...\n" + myName);
+
+            var pi = 3.14;
+            Console.WriteLine("\nThis variable was assigned using \"var\":\npi = " + pi + "\n");
+                
+            new ConstructIt();
+            new ConstructIt("truck");
+            new ConstructIt("truck", "red");
+
+            Console.Read();
+        }
+    }
+}
