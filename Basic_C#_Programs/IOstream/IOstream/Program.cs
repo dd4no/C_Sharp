c445181af3ac11917532ea3a146f8ed5f80c0805
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-12 08:14:59 -0500

    Add new project: IOstreams
    Demonstrates logging to and retrieving from  a text file.

---
+++ Basic_C#_Programs/IOstream/IOstream/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOstream
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a number:");
+            //string number = Console.ReadLine();
+            using (StreamWriter number = new StreamWriter(@"C:\Users\daved\Documents\GitHub\C_Sharp\Basic_C#_Programs\IOstream\log.txt", true))
+            {
+                number.WriteLine(Console.ReadLine());
+            }
+            string readBack = File.ReadAllText(@"C:\Users\daved\Documents\GitHub\C_Sharp\Basic_C#_Programs\IOstream\log.txt");
+            Console.WriteLine("\nYou have entered the number(s):\n" + readBack);
+            Console.ReadLine();
+
+        }
+    }
+}
