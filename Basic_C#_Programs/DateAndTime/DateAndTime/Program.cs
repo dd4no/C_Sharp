04a8abb1b65f96eb4d4fe1565915cfecee2b1767
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-12 08:53:07 -0500

    Add new project: DateAndTime
    Demonstrates DateTime object use and time calculation.

---
+++ Basic_C#_Programs/DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = new DateTime();
+            time = DateTime.Now;
+
+            Console.WriteLine("The current date and time is:");
+            Console.WriteLine(time + "\n");
+
+            Console.WriteLine("Please enter hours into the future:");
+            double hours = Convert.ToDouble(Console.ReadLine());
+
+            //DateTime futureTime = new DateTime();
+            DateTime futureTime = time.AddHours(hours);
+            Console.WriteLine("\nIn {0} hours it will be:", hours);
+            Console.WriteLine(futureTime);
+
+            Console.ReadLine();
+        }
+    }
+}
