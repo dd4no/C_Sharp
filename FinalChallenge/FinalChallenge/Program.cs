983b33c004859368fb34688508dff92f8fa320ec
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-02-24 21:19:20 -0500

    Add new project: FinalChallenge
    Demonstrates a basic Entity Framework code-first app that inserts one record upon construction.

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                var student = new Student() { FirstName = "Dave", LastName = "Defourneaux" };
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual Grade Grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public string GradeLetter { get; set; }
+        public string Course { get; set; }
+        public virtual List<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
